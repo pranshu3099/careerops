69583df8e932383b8bd5ae0f9339dddc948f27fb
Pranshu Srivastava <srivastavapranshu2@gmail.com>
2026-04-19 19:06:07 +0530

    set token on login and initialize refresh in _app on load

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,19 @@
+import { refreshAccessToken } from "@/lib/api";
 import "@/styles/globals.css";
+import { useEffect, useState } from "react";
 import { Toaster } from "react-hot-toast";
 export default function App({ Component, pageProps }) {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const initAuth = async () => {
+      await refreshAccessToken();
+      setLoading(false);
+    };
+
+    initAuth();
+  }, []);
+
+  if (loading) return <div>Loading...</div>;
   return (
     <>
       <Toaster position="top-center" />
